3db304dd4a310bd076a1ba6d09cf89f6b888c8b4
Pradeep Kurapati <kurapatipradeep0@gmail.com>
2023-02-27 22:45:03 +0530

    Day 18: Improved Code for Flashy.

--- Day 18/Flashy/main.py
+++ Day 18/Flashy/main.py
@@ -1,64 +1,67 @@
 from tkinter import *
-from random import *
 import pandas
-
-# -------------- Constants -------------- #
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
-global current_card
-# ------------- French Word Generation ---------------- #
-data = pandas.read_csv('data/french_words.csv')
-to_learn = data.to_dict(orient='records')
+current_card = {}
+to_learn = {}
+
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    print(original_data)
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
 
 
-def next_word():
+def next_card():
     global current_card, flip_timer
     window.after_cancel(flip_timer)
-    current_card = choice(to_learn)
-    print(current_card)
-    canvas.itemconfig(background_image, image=my_image_1)
-    canvas.itemconfig(card_title, text='French', fill='black')
-    canvas.itemconfig(card_word, text=current_card['French'], fill='black')
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
     flip_timer = window.after(3000, func=flip_card)
 
 
 def flip_card():
-    canvas.itemconfig(card_title, text='English', fill='white')
-    canvas.itemconfig(card_word, text=current_card['English'], fill='white')
-    canvas.itemconfig(background_image, image=back_image)
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
 
 
-# -------------- Window -------------- #
+def is_known():
+    to_learn.remove(current_card)
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
+
+
 window = Tk()
+window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
-window.title('Flashy')
-window.config(width=850, height=530)
 
 flip_timer = window.after(3000, func=flip_card)
 
-# Canvas
-canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
-my_image_1 = PhotoImage(file='images/card_front.png')
-background_image = canvas.create_image(400, 263, image=my_image_1)
-canvas.grid(row=1, column=1, columnspan=2)
+canvas = Canvas(width=800, height=526)
+card_front_img = PhotoImage(file="images/card_front.png")
+card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0, columnspan=2)
 
-# Correct button
-right_image = PhotoImage(file='images/right.png')
-Known_button = Button(image=right_image, highlightthickness=0, bg=BACKGROUND_COLOR, command=next_word)
-Known_button.grid(row=2, column=1)
+cross_image = PhotoImage(file="images/wrong.png")
+unknown_button = Button(image=cross_image, highlightthickness=0, command=next_card)
+unknown_button.grid(row=1, column=0)
 
-# Wrong Button
-wrong_image = PhotoImage(file='images/wrong.png')
-unknown_button = Button(image=wrong_image, highlightthickness=0, bg=BACKGROUND_COLOR, command=next_word)
-unknown_button.grid(row=2, column=2)
+check_image = PhotoImage(file="images/right.png")
+known_button = Button(image=check_image, highlightthickness=0, command=is_known)
+known_button.grid(row=1, column=1)
 
-# writing French
-card_title = canvas.create_text(400, 150, text='Title', font=('Ariel', 40, 'italic'))
-card_word = canvas.create_text(400, 263, text='word', font=('Ariel', 60, 'bold'))
-
-# Back Image
-back_image = PhotoImage(file='images/card_back.png')
-
-next_word()
+next_card()
 
 window.mainloop()
