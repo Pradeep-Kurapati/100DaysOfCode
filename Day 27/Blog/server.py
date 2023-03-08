bbdc6463cde58f68b41f709969b2c213a1eb1917
Pradeep Kurapati <kurapatipradeep0@gmail.com>
2023-03-08 18:15:49 +0530

    Day 27: Using Loops and conditions in Jinja

---
+++ Day 27/Blog/server.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+
+@app.route("/blog")
+def blog():
+    blog_url = "https://api.npoint.io/7ae592855be9b6823cff"
+    response = requests.get(url=blog_url)
+    data = response.json()
+    return render_template("index.html", data=data)
